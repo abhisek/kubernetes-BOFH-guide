e8a35f4858d74041a44e2c2cc827428529c130de
abhisek <abhisek.datta@gmail.com>
2019-12-13 22:17:20 +0530

    Updated

--- tunnels/ssh/start.sh
+++ tunnels/ssh/start.sh
@@ -4,7 +4,9 @@
 cat $SSH_PRIVATE_KEY > /root/skey
 chmod 400 /root/skey
 
-ssh -T -N -vvv -o StrictHostKeyChecking=no \
+ssh -T -N -vvv \
+	-o ServerAliveInterval=180 \
+	-o StrictHostKeyChecking=no \
 	-i /root/skey \
 	-R 0.0.0.0:$FWD_PORT:ingress-nginx.ingress-nginx.svc.cluster.local:$FWD_PORT $SSH_USER@$SSH_SERVER
 
