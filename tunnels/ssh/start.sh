590b8e17ad8cbee43e452b7bea049b981c81cd4b
abhisek <abhisek.datta@gmail.com>
2020-01-24 20:29:27 +0530

    Added liveness probe

--- tunnels/ssh/start.sh
+++ tunnels/ssh/start.sh
@@ -4,12 +4,14 @@
 cat $SSH_PRIVATE_KEY > /root/skey
 chmod 400 /root/skey
 
-ssh -T -N -vvv \
-	-o ServerAliveInterval=180 \
-	-o StrictHostKeyChecking=no \
+export AUTOSSH_GATETIME=0
+export AUTOSSH_PIDFILE="/tmp/autossh.pid"
+export AUTOSSH_DEBUG=1
+
+autossh -M 0 -T -N \
+	-o "ServerAliveInterval 10" \
+	-o "ServerAliveCountMax 3" \
+	-o "StrictHostKeyChecking no" \
 	-i /root/skey \
 	-R 0.0.0.0:$FWD_PORT:ingress-nginx.ingress-nginx.svc.cluster.local:$FWD_PORT $SSH_USER@$SSH_SERVER
-
-
-
 
